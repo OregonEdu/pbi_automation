b61215c7dfa9bbfe1f2034ecb01c0718c6262c53
bearshark <bearshark@ode.oregon.gov>
2026-02-05 15:36:07 -0800

    Initial release - v1.0
    Power BI Automation scripts

---
+++ PBI_Workspace_Audit.ps1
@@ -0,0 +1,165 @@
+﻿## Power BI Workspace Audit
+## Benjamin Barshaw <benjamin.barshaw@ode.oregon.gov> - IT Operations & Support Network Team Lead - Oregon Department of Education
+#
+#  Requirements: MicrosoftPowerBIMgmt PowerShell Module (Install-Module -Name MicrosoftPowerBIMgmt)
+#                Fabric/PowerBI Administrator PIM Role
+#                Must be connected to the tenant's Power BI space with Login-PowerBIServiceAccount
+#
+#  This script was created to determine which Power BI workspaces in our tenant were no longer being used. We use the Get-PowerBIActivityEvent cmdlet to retrieve activity logs going back as far as we can go. The cmdlet does not allow you to select date ranges -- you MUST
+#  use the same day in the StartDateTime and EndDateTime parameters. To get all logs for a given day we set our StartDateTime for midnight (00:00:00) and our EndDateTime for 1 minute 'til midnight (23:59:59).  The cmdlet is also tricky in that if you select a date out of the 
+#  range that it still has data for, it does not give you any useful sort of error it just fails with "Get-PowerBIActivityEvent: Operation returned an invalid status code 'BadRequest'".  For this reason we set a $tryStartDate variable with how many days we wish to attempt to
+#  go back and then decrement that integer until we get to 0 pulling logs for each date (30 days ago, 29 days ago, 28 days ago, ad nauseam) as we make our way to today's date. We then retrieve all NON-PERSONAL Power BI workspaces in the tenant and query the logs we pulled for
+#  any activity in them. If found, the CSV we export will show the most recent time of the last activity found. We also export any users belonging to the workspace by the access type they possess -- Admin, Contributor, Member, Viewer.  The CSV will be exported to the Current
+#  Working Directory (CWD) as PBI_Workspace_Audit-<MMddyyyy>.csv with MMddyyyy being today's date.
+#  
+### USER DEFINED VARIABLES SECTION ###
+# I wouldn't change this, but here is where we define how far back we would care to try
+$tryStartDate = 30
+### END USER DEFINED VARIABLES SECTION ###
+
+# Create the columns of our CSV export -- originally I had lumped all users into one column but by request split them up into access types
+class PBIWorkspace
+{
+    [string]$WorkspaceName
+    [string]$WorkspaceId
+    [string]$State
+    [string]$LastUsed    
+    #[string]$Users # Commented out but left in for posterity
+    [string]$Admins
+    [string]$Contributors
+    [string]$Members
+    [string]$Viewers
+}
+
+# Get today's date in a format for the CSV
+$exportDate = Get-Date -Format MMddyyyy
+# Get today's date in a format for Get-Date
+$getToday = Get-Date -Format MM/dd/yyyy
+# If we just did Get-Date it would return the date with the current time -- by specifying -Date $getToday for today's date it makes the time start at midnight since we want the whole day's worth of activity
+$getDate = Get-Date -Date $getToday
+# Initialize the array that will house ALL the logs
+$getPBILogs = @()
+
+$version = "v1.0"
+
+# If we are connected to the Power BI space we'll get a token (unused) and keep going. If we are NOT connected it will throw an error and attempt to login to the Power BI space. It's tricky, tricky, tricky - Run DMC
+try
+{
+    $areWeConnected = Get-PowerBIAccessToken
+}
+catch
+{
+    Login-PowerBIServiceAccount
+    $areWeConnected = Get-PowerBIAccessToken
+}
+
+# Exit if we are unable to login to Power BI space
+If (! $areWeConnected)
+{
+    Write-Host -ForegroundColor Red "We are not connected to the Power BI space in the tenant! Exiting..."
+    exit
+}
+
+Write-Host -ForegroundColor Cyan "Power BI Workspace Audit Script $($version) - Benjamin Barshaw <benjamin.barshaw@ode.oregon.gov>"
+Write-Host -ForegroundColor Cyan "Getting all non-personal Power BI workspaces in tenant..."
+# Retrieve all the Power BI workspaces that aren't "PersonalGroup" -- these are personal Power BI spaces and we aren't interested in them for the audit. If we were interested in those we would simpy remove there Where-Object
+$getPBIWorkspaces = Get-PowerBIWorkspace -Scope Organization -All | Where-Object { $_.Type -ne "PersonalGroup" }
+Write-Host -ForegroundColor Magenta "Found $($getPBIWorkspaces.Count) workspaces!"
+
+# Start at 30 days ago and work our way down to 0
+For ($i = $tryStartDate; $i -ge 0; $i--)
+{
+    Write-Host -ForegroundColor DarkCyan "Getting workspace activity from $($i) days ago..."
+    # Here we go back $i days at midnight    
+    $startDate = ($getDate).AddDays(-$i)
+    # Here we go back $i days at 1 minute 'til midnight
+    $endDate = Get-Date -Date $startDate -Hour 23 -Minute 59 -Second 59
+    # Oh yeah - this cmdlet is a REAL JERK about the format of the time you give it. I found out after I came up with this that the same thing could have been accomplished with (Get-Date -Date $startDate).ToString("s") -- live and learn
+    $startTime = (Get-Date -Format "o" -Date ($startDate).ToString()).Split('.')[0]
+    $endTime = (Get-Date -Format "o" -Date ($endDate).ToString()).Split('.')[0]
+
+    try
+    {
+        # Retrieve the logs for the day
+        $getJson = Get-PowerBIActivityEvent -StartDateTime $startTime -EndDateTime $endTime -ErrorAction Stop
+        # Add all the logs for the day to our array
+        $getPBILogs += $getJson
+    }
+    catch
+    {
+        # I'm fairly confident you'll get at least one of these if you kept the $tryStartDate variable at 30
+        Write-Host -ForegroundColor Red "Problem getting Power BI Activity Events from $($i) days ago!"
+    }    
+}
+
+# Convert our JSON to PowerShell objects for searching
+$getSearchable = $getPBILogs | ConvertFrom-Json
+
+# Cycle through all retrieved PBI workspaces
+ForEach ($pbiWorkspace in $getPBIWorkspaces)
+{
+    Write-Host -ForegroundColor Yellow "Working on $($pbiWorkspace.Name)..."
+    
+    # Create arrays for all the potential various access rights -- we'll add users into their respective array
+    $adminsTempArray = @()
+    $contributorsTempArray = @()
+    $membersTempArray = @()
+    $viewersTempArray = @()
+
+    # Create our class object and start populating it with data retrieved from the Get-PowerBIWorkspace cmdlet
+    $exportMe = [PBIWorkspace]::new()
+    $exportMe.WorkspaceName = $pbiWorkspace.Name
+    $exportMe.WorkspaceId = $pbiWorkspace.Id
+    $exportMe.State = $pbiWorkspace.State
+    
+    # Try to find activity in our large log array for the current workspace
+    $findWorkspace = $getSearchable | Where-Object { $_.WorkspaceName -eq $pbiWorkspace.Name }
+    If ($findWorkspace)
+    {
+        # If we find activity/activities, we sort them and use [-1] to get the LAST object which after sorting should be the most recent
+        $exportMe.LastUsed = ($findWorkspace.CreationTime | Sort-Object)[-1]
+    }
+    Else
+    {
+        # If no activity found just put a dash
+        $exportMe.LastUsed = "-"
+    }
+    
+    # Cycle through all the users found for the workspace
+    ForEach ($pbiUser in $pbiWorkspace.Users)
+    {
+        # We use the access right (permission) to add the users into their respective array
+        switch ($pbiUser.AccessRight)
+        {
+            "Admin"
+            {
+                $adminsTempArray += $pbiUser.UserPrincipalName
+            }
+            "Contributor"
+            {
+                $contributorsTempArray += $pbiUser.UserPrincipalName
+            }
+            "Member"
+            {
+                $membersTempArray += $pbiUser.UserPrincipalName
+            }
+            "Viewer"
+            {
+                $viewersTempArray += $pbiUser.UserPrincipalName
+            }
+        }
+    }
+
+    # This was when I had them all in one column -- left for posterity
+    #$exportMe.Users = $pbiWorkspace.Users.UserPrincipalName -join ","
+    
+    # We take any users found in the access right arrays and join them together into a string separating them with a comma
+    $exportMe.Admins = $adminsTempArray -join ", "
+    $exportMe.Contributors = $contributorsTempArray -join ", "
+    $exportMe.Members = $membersTempArray -join ", "
+    $exportMe.Viewers = $viewersTempArray -join ", "    
+
+    # Export our populated PBIWorkSpace class object to the CSV
+    $exportMe | Export-Csv -Append -NoTypeInformation -Path ".\PBI_Workspace_Audit-$exportDate.csv"
+}
+# FIN
---
+++ PBI_Workspace_Deletion.ps1
@@ -0,0 +1,160 @@
+﻿## Power BI Workspace Deletion Script
+## Benjamin Barshaw <benjamin.barshaw@ode.oregon.gov> - IT Operations & Support Network Team Lead - Oregon Department of Education
+#
+#  Requirements: MicrosoftPowerBIMgmt PowerShell Module (Install-Module -Name MicrosoftPowerBIMgmt)
+#                Fabric/PowerBI Administrator PIM Role
+#                Must be connected to the tenant's Power BI space with Login-PowerBIServiceAccount
+#
+#  This script was created as Microsoft does not make it easy for administrator to delete a Power BI Workspace from the Admin Console. You must first add your Administrator account as an Owner, then go to the workspace, go to the settings, scroll down to the bottom and hit
+#  "Remove". They do not have a PowerShell cmdlet (yet) that deletes them either, so you must use the Power BI REST API interface to script any sort of mass deletions. I was quite pleased to find out that they have a cmdlet "Get-PowerBIAccessToken" however as it meant not
+#  having to add in my functions for generating a device code to get a token. Thanks Microsoft! This script is menu-driven and does make you confirm before deleting a Workspace to avoid tears.
+#  
+### USER DEFINED VARIABLES SECTION ###
+# I wouldn't change this, but here is where we define how far back we would care to try
+$paAccount = "<insert_PA_account_UPN>"
+### END USER DEFINED VARIABLES SECTION ###
+
+$version = "v1.0"
+
+# Function to add the PA account defined in the above $paAccount variable as an Owner of the PBI Workspace
+function addAdminToPBIWorkspace($pbiWorkspaceName, $pbiWorkspaceId)
+{    
+    Write-Host -ForegroundColor Cyan "Adding $paAccount to $pbiWorkspaceName with workspaceId $pbiWorkspaceId"
+
+    $body = @{
+        emailAddress = $paAccount
+        groupUserAccessRight = "Admin"
+    } | ConvertTo-Json
+
+    Invoke-RestMethod -Method POST -Uri "https://api.powerbi.com/v1.0/myorg/admin/groups/$pbiWorkspaceId/users" -Headers $getHeaders -Body $body -ContentType "application/json"
+
+    Write-Host -ForegroundColor Magenta "Added!"
+}
+
+# Function to delete the PBI Workspace with prompting to confirm you really wish to delete it
+function deletePBIWorkspace($pbiWorkspaceName, $pbiWorkspaceId)
+{
+    Write-Host -ForegroundColor DarkYellow "Are you sure you want to delete $workspaceName"
+    $yesNo = $null
+    while (($yesNo -ne 'y') -and ($yesNo -ne 'n')) 
+    {
+        $yesNo = Read-Host -Prompt "[Y/N]"
+    }
+    
+    If ($yesNo -eq "y")
+    {    
+        Write-Host -ForegroundColor Cyan "Deleting $pbiWorkspaceName with workspaceId $pbiWorkspaceId"
+
+        Invoke-RestMethod -Method DELETE -Uri "https://api.powerbi.com/v1.0/myorg/groups/$pbiWorkspaceId" -Headers $getHeaders
+
+        Write-Host -ForegroundColor Magenta "Deleted!"
+    }
+}
+
+# Display the menu with all our options for deletions
+function showMenu
+{
+    Write-Host -ForegroundColor Cyan "Power BI Workspace Deletion Script $($version) - Benjamin Barshaw <benjamin.barshaw@ode.oregon.gov>"
+    Write-Host -ForegroundColor Yellow "Please make your selection:"
+    Write-Host -ForegroundColor DarkYellow "1) Delete single Power BI workspace"
+    Write-Host -ForegroundColor DarkYellow "2) Delete orphaned Power BI workspaces"
+    Write-Host -ForegroundColor DarkYellow "3) Delete all non-personal Power BI workspaces (CAUTION!)"
+    Write-Host -ForegroundColor DarkYellow "4) Delete all Power BI workspaces from CSV file (Must have WorkspaceName & WorkspaceId headers)"
+    Write-Host -ForegroundColor DarkYellow "5) Show menu"
+    Write-Host -ForegroundColor DarkYellow "6) Exit"
+}
+
+Write-Host -ForegroundColor Magenta "Checking if we are connected to Power BI..."
+
+# Check to see if we can get PBI token -- if not, login and try again!
+try
+{
+    $getHeaders = Get-PowerBIAccessToken
+}
+catch
+{
+    Login-PowerBIServiceAccount
+    $getHeaders = Get-PowerBIAccessToken
+}
+
+showMenu
+
+do
+{
+    $menuResponse = (Read-Host -Prompt "Choice (4 to re-display menu; 5 to exit)") -as [int]
+
+    switch($menuResponse)
+    {
+        # Menu option 1 - delete a single PBI workspace specified by name -- will lookup the ID for you!
+        1
+        {
+            $workspaceName = $(Read-Host -Prompt "Workspace name")
+            Write-Host -ForegroundColor Yellow "Looking up workspace ID for $workspaceName)..."
+            If (($getWorkspaceId = (Get-PowerBIWorkspace -Name $workspaceName -Scope Organization).Id) -eq $null)
+            {
+                Write-Host -ForegroundColor Red "Could not find Power BI workspace $workspaceName!"
+            }
+            Else
+            {
+                addAdminToPBIWorkspace $workspaceName $getWorkspaceId
+                deletePBIWorkspace $workspaceName $getWorkspaceId                
+            }
+        }
+        # Menu option 2 - retrieve all the orphaned PBI workspaces in the tenant -- these are even trickier to do in the webUI
+        2
+        {
+            Write-Host -ForegroundColor Yellow "Retrieving all orphaned Power BI workspaces for tenant..."
+            $getOrphanedWorkspaces = Get-PowerBIWorkspace -Scope Organization -Orphaned
+            ForEach ($orphanedWorkspace in $getOrphanedWorkspaces)
+            {
+                addAdminToPBIWorkspace $($orphanedWorkspace).Name $($orphanedWorkspace).Id
+                deletePBIWorkspace $($orphanedWorkspace).Name $($orphanedWorkspace).Id
+            }
+        }
+        # Menu option 3 - retrive ALL non-personal and not in state of deletion PBI workspaces in the tenant for deletion -- USE THIS ONE WITH CAUTION!
+        3
+        {
+            Write-Host -ForegroundColor Yellow "Retrieving all non-personal Power BI workspaces for tenant..."
+            $getAllNonPersonalWorkspaces = Get-PowerBIWorkspace -Scope Organization -All | Where-Object { $_.Type -ne "PersonalGroup" -and $_.State -ne "Deleted" }
+            ForEach ($nonPersonalWorkspace in $getAllNonPersonalWorkspaces)
+            {
+                addAdminToPBIWorkspace $($nonPersonalWorkspace).Name $($nonPersonalWorkspace).Id
+                deletePBIWorkspace $($nonPersonalWorkspace).Name $($nonPersonalWorkspace).Id
+            }
+        }
+        # Meny option 4 - input a CSV file (like the one output from PBI audit script) for mass deletion
+        4
+        {
+            $getCsv = $(Read-Host -Prompt "Workspace CSV (Must have WorkspaceName & WorkspaceId headers)")
+            $workspaceCsv = Import-Csv -Path $getCsv -ErrorAction SilentlyContinue
+            If ($workspaceCsv -eq $null)
+            {
+                Write-Host -ForegroundColor Red "Problem importing CSV $getCsv!"
+            }
+            Else
+            {
+                Write-Host -ForegroundColor Yellow "Successfully imported $getCsv! Retrieving all Power BI workspaces..."
+                ForEach ($pbiWorkspace in $workspaceCsv)
+                {
+                    addAdminToPBIWorkspace $($pbiWorkspace).WorkspaceName $($pbiWorkspace).WorkspaceId
+                    deletePBIWorkspace $($pbiWorkspace).WorkspaceName $($pbiWorkspace).WorkspaceId
+                }
+            }
+        }
+        # Menu option 5 - redisplay the menu
+        5
+        {
+            showMenu
+        }
+        # Menu option 6 - get the hell outta Dodge
+        6
+        {
+            exit
+        }
+        default
+        {
+            Write-Host -ForegroundColor Red "Invalid option!"
+        }
+    }
+} 
+while ($menuResponse -ne 6)
